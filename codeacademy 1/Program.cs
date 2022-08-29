60d1634ac59e435c428beb9ad7d3c03d1e1b5e30
jafar_hamza <jafar_hamza@G-02-11>
2022-08-29 10:02:48 -0400

    Add project files.

---
+++ codeacademy 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeacademy_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+
+            //yo wassa
+        }
+    }
+}
